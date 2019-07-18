24adda3a01a422a2b40d57f98332c03a0a72dbc7
Nazli Cingisiz <cingisiznazli@gmail.com>
2019-07-18 19:02:08 +0300

    test

---
+++ Kisi.java
@@ -0,0 +1,25 @@
+
+package veri;
+
+public class Kisi {
+   
+    String isim;
+    
+    public Kisi()
+    {
+        isim=null;
+       System.out.println("Kisi objesi olusturuldu");
+              
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+    
+    
+}
+
